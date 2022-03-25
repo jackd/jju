ef8d9da861f6735dce73432a3266bcc246ebf787
Emmanuel Sérié <eserie@gmail.com>
2022-03-26 00:32:36 +0100

    add notebooks

---
+++ docs/notebooks/00_scipy.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py,md
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Locally Optimal Block Preconditioned Conjugate Gradient (LOBPCG) method
+#
+# From [wikipedia page](https://en.wikipedia.org/wiki/LOBPCG):
+# > LOBPCG is a matrix-free method for finding the largest (or smallest) eigenvalues and the corresponding eigenvectors of a symmetric generalized eigenvalue problem.
+#
+#     A x = λ B x
+#
+#
+# I found the method following this recent discussion:
+# [What is the most efficient method for calculating eigenvalues and eigenvectors of large matrices?](https://www.researchgate.net/post/What-is-the-most-efficient-method-for-calculating-eigenvalues-and-eigenvectors-of-large-matrices)
+#
+# In particular, Andrew Knyazev, the author of the LOBPCG, makes multiple interesting remarks.
+#
+#
+# He talks about the  LOBPCG  method:
+# > For example, LOBPCG works only for symmetric problems, including
+# > generalized, to find <5-10% of extreme eigenpairs, but whether 
+# > the matrix is sparse or dense is not so important.
+# > For a full review of options, please read this [paper from 1997](https://www.researchgate.net/publication/46619332_Templates_for_the_Solution_of_Algebraic_Eigenvalue_Problems_A_Practical_Guide).
+#
+#
+# >The already mentioned LOBPCG has many implementations. In MATLAB, including support for single precision, distributed or codistributed arrays, and tiling arrays, please see
+# https://www.mathworks.com/matlabcentral/fileexchange/48-locally-optimal-block-preconditioned-conjugate-gradient .
+# A plain C version is in https://github.com/lobpcg/blopex .
+# In other cases, LOBPCG is already included in the hosting package, e.g., Anasazi (Trilinos), SLEPc, hypre, SciPy, Octave, Julia, MAGMA, Pytorch, Rust, RAPIDS cuGraph, and NVIDIA AMGX. For a comprehensive review, see https://en.wikipedia.org/wiki/LOBPCG
+#
+#
+# We can find an implementation in scipy:
+# - https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.lobpcg.html
+#
+# And in pytorch:
+# - https://pytorch.org/docs/stable/generated/torch.lobpcg.html
+
+
+# Let's work with some examples.
+
+# %load_ext autoreload
+# %autoreload 2
+
+import numpy as np
+import scipy.linalg
+from scipy.sparse import issparse, spdiags
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse.linalg import lobpcg
+
+
+def generate_wishart(N=1000, T=1100):
+    X = np.random.randn(T, N)
+    W = X.T @ X / T
+    return W, X
+
+
+rng = np.random.default_rng()
+
+N = 5000
+T = 100
+
+W, X = generate_wishart(N, T)
+X.shape
+
+X0 = rng.random((N, 3))
+
+# %time ev, X1 = lobpcg(W, X0)
+ev
+
+# Let's look if we gain in speed if we use the warm start mechanism of LOBPCG:
+
+for i in range(5):
+    # %time ev, X0 = lobpcg(W, X0)
+ev
+
+# Now Let's try to exploit the structure of the matrix and the fact that in our current example the data matrix $X$ is very thin.
+
+A = LinearOperator(matvec=lambda v: W @ v, shape=(N, N), dtype=float)
+
+X0 = rng.random((N, 3))
+# %time ev, X1 = lobpcg(A, X0)
+ev
+
+A = LinearOperator(
+    matvec=lambda v: (X.T @ (X @ v)) / T,
+    # rmatvec=lambda v: (X.T @ (X @ v))/T,
+    # matmat=lambda v: (X.T @ (X @ v))/T,
+    shape=(N, N),
+    dtype=float,
+)
+
+X0 = rng.random((N, 3))
+# %time ev, X1 = lobpcg(A, X0)
+ev
+
+for i in range(6):
+    # %time ev, X0 = lobpcg(A, X0)
+ev
+
+# We end with a very small execution time!
+
+# Can we use preconditionner to speedup again things
+
+# Initial guess for eigenvectors, should have linearly independent
+# columns. Column dimension = number of requested eigenvalues.
+#
+
+# Preconditioner in the inverse of A in this example:
+#
+
+invA = spdiags([1.0 / (X**2).mean(0)], 0, N, N)
+
+# The preconditiner must be defined by a function.
+
+# The argument x of the preconditioner function is a matrix inside `lobpcg`, thus the use of matrix-matrix product ``@``.
+#
+# The preconditioner function is passed to lobpcg as a `LinearOperator`:
+#
+
+M = LinearOperator(matvec=lambda v: invA @ v, shape=(N, N), dtype=float)
+
+# Let us now solve the eigenvalue problem for the matrix A:
+#
+
+rng = np.random.default_rng()
+X0 = rng.random((N, 3))
+
+# %time ev, X1 = lobpcg(A, X0, M=M, largest=True)
+ev
+
+for i in range(6):
+    # %time ev, X0 = lobpcg(A, X0, M=M, largest=True)
+X0
+
+# Constraints:
+
+# Note that the vectors passed in Y are the eigenvectors of the 3 smallest
+#     eigenvalues. The results returned are orthogonal to those.
+
+# The matrix constraint should be used when solving the eigen problem while embending the problem in a larger dimensional space.
+
+Y = np.eye(N, 5)
+
+rng = np.random.default_rng()
+X0 = rng.random((N, 3))
+
+# %time ev, X1 = lobpcg(A, X0, Y=Y, M=M, largest=True)
+ev
+
+for i in range(6):
+    # %time ev, X0 = lobpcg(A, X0, Y=Y, M=M, largest=True)
+X0
+
+for i in range(6):
+    # %time ev, X0 = lobpcg(W, X0, Y=Y, largest=True)
+X0
+
+# When constraints are specified, then warm start does not matter anymore.
---
+++ docs/notebooks/01_LOBPCG_JAX.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light,md
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+#
+# # JAX implementations
+#
+# JAX expose numpy and scipy implementations of `eigh` function but at the moment, advanced options such as `eigvals` are not available.
+#
+# Here are some interesting links:
+# - [jackd/jju: Jack's Jax Utilities](https://github.com/jackd/jju)
+#
+# In particular, it contains a JAX implementation of LOBPCG discussed above:
+# - https://github.com/jackd/jju/blob/master/jju/linalg/lobpcg/basic.py#L11
+#
+# The issue https://github.com/google/jax/issues/3112 from JAX's Github is also discussing an implementation.
+#
+# ### Benchmarking with numpy
+#
+# I found this study:
+# https://towardsdatascience.com/turbocharging-svd-with-jax-749ae12f93af?gi=398628dbfc88
+# which compare jax and numy algorithms.
+#
+# There is in the end no big difference between jax and numpy from the speed perspective.
+#
+#
+# Other benchmarking results can be found in this study:
+# - [Separate your filters! Separability, SVD and low-rank approximation of 2D image processing filters | Bart Wronski](https://arxiv.org/pdf/2009.07542.pdf)
+#
+# This recent paper (2019):
+# - [Differentiable Programming Tensor Networks
+# Hai-Jun Liao, Jin-Guo Liu, Lei Wang, Tao Xiang](https://arxiv.org/abs/1903.09650).
+# discuss which implementations of eigh may be recommended when used in differentiable pipelines.
+# They have an implementation of differentiable SVD in Pytorch.
+
+# %load_ext autoreload
+# %autoreload 2
+
+# +
+import jax
+import jax.numpy as jnp
+import scipy.sparse
+import numpy as onp
+
+from jju.linalg.lobpcg.basic import lobpcg
+from jju.linalg.lobpcg.utils import identity, rayleigh_ritz
+from jju.types import as_array_fun
+# -
+
+T = 100
+N = 5000
+
+rng = jax.random.PRNGKey(42)
+X = jax.random.uniform(rng, (T, N))
+W = X.T @ X / T
+X0 = jax.random.uniform(rng, (N, 3))
+
+
+# ## test LOBPCG
+
+X0 = jax.random.uniform(rng, (N, 3))
+
+
+# %time ev, _ = scipy.sparse.linalg.lobpcg(onp.array(W).astype(onp.float64), onp.array(X0).astype(onp.float64))
+ev
+
+Y = None
+
+
+# +
+# ??lobpcg
+
+# +
+rng = jax.random.PRNGKey(42)
+X = jax.random.uniform(rng, (T, N))
+W = X.T @ X / T
+X0 = jax.random.uniform(rng, (N, 3))
+
+# %time ev, X1 = lobpcg(W, X0, None, None, None, True, None, None, 1000); ev.block_until_ready(); ev.block_until_ready()
+ev
+# -
+
+# Second run is faster:
+
+# %time ev, X1 = lobpcg(W, X0, None, None, None, True, None, None, 1000); ev.block_until_ready(); ev.block_until_ready()
+ev
+
+# ### Warm-start
+
+# %time ev, X1 = lobpcg(W, X1, None, None, None, True, None, None, 1000); ev.block_until_ready(); ev.block_until_ready()
+
+# ## Transpose approach
+
+# +
+rng = jax.random.PRNGKey(42)
+X = jax.random.uniform(rng, (T, N))
+Wsmall = X @ X.T / T
+X0 = jax.random.normal(rng, (T, 3))
+
+# %time ev, X1 = lobpcg(Wsmall, X0, None, None, None, True, None, None, 1000); ev.block_until_ready(); ev.block_until_ready()
+ev
+# -
+
+# %time ev, X1 =  lobpcg(Wsmall, X0, None, None, None, True, None, None, 1000); ev.block_until_ready()
+
+nx = len(ev)
+
+# %time Uhat, s, V = jax.scipy.linalg.svd(X); Uhat.block_until_ready();
+
+Q = X1
+B = Q.T @ X
+# %time Uhat, s, V = jax.scipy.linalg.svd(B); Uhat.block_until_ready()
+U = Q @ Uhat
+U.shape
+U = V[:nx].T
+U.T @ U
+
+Cov = X.T @ X / T
+jnp.diag(U.T @ Cov @ U)
+
+ev
+
+# ### test with constraints
+
+N, T = 5000, 100
+rng = jax.random.PRNGKey(42)
+def generate_wishart(N=1000, T=1100):
+    X = jax.random.normal(rng, (T, N))
+    W = X.T @ X / T
+    return W, X
+W, X = generate_wishart(N, T)
+X0 = jax.random.normal(rng, (N, 3))
+Y = jnp.eye(N, 5)
+
+Y
+
+invA = jnp.diag(1.0 / (X**2).mean(0))
+
+# %time ev, X1 =  lobpcg(jnp.array(W), jnp.array(X0), Y=jnp.array(Y),largest=True, max_iters=39); ev.block_until_ready();#iK=invA, 
+X1
+
+ev
+
+# Starting with max_iters=40 the algorithm give nans.
+# We have to fix it.
+
+# from jax.config import config; config.update("jax_enable_x64", False)
+# %time ev, X1 =  lobpcg(jnp.array(W, "float32"), jnp.array(X0, "float32"), Y=jnp.array(Y, "float32"),largest=True, max_iters=40); ev.block_until_ready();#iK=invA, 
+X1
+
+from jax.config import config; config.update("jax_enable_x64", True)
+
+# %%time 
+ev, X1 =  lobpcg(jnp.array(W, "float64"), jnp.array(X0, "float64"), Y=jnp.array(Y, "float64"), largest=True, max_iters=40); ev.block_until_ready();
+#iK=invA, 
+X1.block_until_ready()
+
+ev
+
+# ## scipy run 
+
+# Let's compare to what gives scipy.
+
+from scipy.sparse.linalg import LinearOperator
+from scipy.sparse import issparse, spdiags
+
+if False:
+    import numpy as np
+    print(N, T)
+    N, T = 5000, 100
+
+    def generate_wishart(N=1000, T=1100):
+        X = np.random.randn(T, N)
+        W = X.T @ X / T
+        return W, X
+    W, X = generate_wishart(N, T)
+    rng = np.random.default_rng()
+    X0 = rng.random((N, 3))
+    Y = np.eye(N, 5)
+
+# %time ev, X1 = scipy.sparse.linalg.lobpcg(onp.array(W), X0, Y=Y, largest=True, maxiter=39)
+X1
+
+ev
+
+# %time ev, X1 = scipy.sparse.linalg.lobpcg(onp.array(W, float), onp.array(X0, float), Y=onp.array(Y, float), largest=True, maxiter=39)
+X1
+
+ev
+
+# It converges better with computations in float64.
